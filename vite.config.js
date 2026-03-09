0bba127020d1eeaaeeb4d7b1a3c887f115f86513
YukiJanse <yuki.janse@gmail.com>
2026-03-09 13:24:41 +0100

    Fix bug

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: "/hotel-booking/"
+  plugins: [react()],
+  base: "/hotel-booking-frontend/" // Ditt repo-namn på GitHub
 })
-
