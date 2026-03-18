70ff915e4dedc59fca6ffbe6ac31298c39fd78f4
YukiJanse <yuki.janse@gmail.com>
2026-03-18 12:11:02 +0100

    Modify text for hotel rooms

--- src/pages/RoomList.jsx
+++ src/pages/RoomList.jsx
@@ -9,7 +9,7 @@ function RoomList() {
                     <img src="https://picsum.photos/300/200" alt="room image photo" />
                     <div className="card-body">
                         <h3 className="card-title">Suite</h3>
-                        <p className="card-text">Exemple text about the room. Exemple text about the room. Exemple text about the room.Exemple text about the room.</p>
+                        <p className="card-text">Lyxigt och rymligt rum med separat vardagsrum, perfekt för en avkopplande vistelse. Njut av hög komfort, modern inredning och en fantastisk utsikt.</p>
                         <Link to="/?roomType=Suite" className='btn btn-primary'>
                             Boka rummet
                         </Link>
@@ -19,7 +19,7 @@ function RoomList() {
                     <img src="https://picsum.photos/300/200" alt="room image photo" />
                     <div className="card-body">
                         <h3 className="card-title">Deluxe</h3>
-                        <p className="card-text">Exemple text about the room. Exemple text about the room. Exemple text about the room.Exemple text about the room.</p>
+                        <p className="card-text">Ett stilrent och bekvämt rum med extra bekvämligheter. Perfekt för både affärsresenärer och semesterfirare som vill ha det lilla extra.</p>
                         <Link to="/?roomType=Deluxe" className='btn btn-primary'>
                             Boka rummet
                         </Link>
@@ -29,7 +29,7 @@ function RoomList() {
                     <img src="https://picsum.photos/300/200" alt="room image photo" />
                     <div className="card-body">
                         <h3 className="card-title">Standard</h3>
-                        <p className="card-text">Exemple text about the room. Exemple text about the room. Exemple text about the room.Exemple text about the room.</p>
+                        <p className="card-text">Ett prisvärt och funktionellt rum med allt du behöver för en bekväm vistelse. Perfekt för kortare resor eller enkla övernattningar.</p>
                         <Link to="/?roomType=Standard" className='btn btn-primary'>
                             Boka rummet
                         </Link>
